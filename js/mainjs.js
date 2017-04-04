f4b8c8c7a2bf0a9aca287016820dcfff13de52b0
Anton Ahl <drtigerace@gmail.com>
2017-04-04 09:39:46 +0200

    javascript för window.onresize

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,20 +1,24 @@
-	(function ($) {
-		Drupal.behaviors.myModuleBehavior = {
-			attach: function (centext, settings) {
-				var menuHeight = $(".region-navigation-collapsible").height();
-				$(".views-field-field-flexslider-bild > div img").one("load", function() {
-					setTimeout(function(){
-					var sliderHeight = $(".views-field-field-flexslider-bild > div").height();
-					if(sliderHeight > (windowHeight - menuHeight)){
-						sliderHeight = windowHeight - menuHeight;
-						$(".views-field-field-flexslider-bild > div").height(sliderHeight);
-						$(".views-field-field-flexslider-bild > div").css("overflow", "hidden");
-					}	
-					}, 0);				
-				}).each(function() {
-					if(this.complete) $(this).load();
-				});
-		}
-	};
+(function ($) {
+	Drupal.behaviors.myModuleBehavior = {
+		attach: function (centext, settings) {
+			sliderH();
+	}
+};
 })(jQuery);
 var windowHeight = window.innerHeight;
+function sliderH(){
+	var menuHeight = $(".region-navigation-collapsible").height();
+		$(".views-field-field-flexslider-bild > div img").one("load", function() {
+		setTimeout(function(){
+			var sliderHeight = $(".views-field-field-flexslider-bild > div").height();
+			if(sliderHeight > (windowHeight - menuHeight)){
+				sliderHeight = windowHeight - menuHeight;
+				$(".views-field-field-flexslider-bild > div").height(sliderHeight);
+				$(".views-field-field-flexslider-bild > div").css("overflow", "hidden");
+			}	
+		}, 10);				
+	}).each(function() {
+		if(this.complete) $(this).load();
+	});
+}
+window.onresize = sliderH();
