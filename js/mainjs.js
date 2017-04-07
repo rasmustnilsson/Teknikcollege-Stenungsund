281bf5795b433f0ea976ca964e23b94dcb176af5
Anton Ahl <drtigerace@gmail.com>
2017-04-07 11:57:09 +0200

    flexslider javascript problem löst

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,24 +1,28 @@
-(function ($) {
-	Drupal.behaviors.myModuleBehavior = {
-		attach: function (centext, settings) {
-			sliderH();
-	}
-};
+	(function ($) {
+		Drupal.behaviors.myModuleBehavior = {
+			attach: function (centext, settings) {
+				var windowHeight = window.innerHeight;
+				var flexsliderCss = "$('.views-field-field-flexslider-bild > div')";
+				function flexSHeight(){
+					var menuHeight = $(".region-navigation-collapsible").height();
+					$(".views-field-field-flexslider-bild > div img").one("load", function() {
+						setTimeout(function(){
+						this.sliderHeight = $(".views-field-field-flexslider-bild > div").height();
+						if(sliderHeight > (windowHeight - menuHeight)){
+							sliderHeight = windowHeight - menuHeight;
+							flexsliderCss.css({"height": sliderHeight, "overflow": "hidden"});
+						} else{
+							flexsliderCss.css({"height": "", "overflow": ""});
+						}
+						}, 0);				
+					}).each(function() {
+						if(this.complete) $(this).load();
+					});	
+				}
+				flexSHeight();
+				$(window).resize(function(){
+					flexSHeight();
+				});
+		}
+	};
 })(jQuery);
-var windowHeight = window.innerHeight;
-function sliderH(){
-	var menuHeight = $(".region-navigation-collapsible").height();
-		$(".views-field-field-flexslider-bild > div img").one("load", function() {
-		setTimeout(function(){
-			var sliderHeight = $(".views-field-field-flexslider-bild > div").height();
-			if(sliderHeight > (windowHeight - menuHeight)){
-				sliderHeight = windowHeight - menuHeight;
-				$(".views-field-field-flexslider-bild > div").height(sliderHeight);
-				$(".views-field-field-flexslider-bild > div").css("overflow", "hidden");
-			}	
-		}, 10);				
-	}).each(function() {
-		if(this.complete) $(this).load();
-	});
-}
-window.onresize = sliderH();
